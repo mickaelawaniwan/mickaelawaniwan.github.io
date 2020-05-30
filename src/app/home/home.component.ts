490512dcfce0e88c6de339d3ea5cc8d1b70d322d
Mickaela Waniwan <mariellewaniwan@Marielles-MacBook-Pro.local>
2020-05-31 01:32:16 +0800

    code cleanup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { map } from "rxjs/operators";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HomeMockData } from '../mock-data';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private http: HttpClient) { }
+  homeData: HomeMockData;
+   ngOnInit() {
+    this.getAllText();
   }
 
+  getAllText(){
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json'});  
+    this.http.get("/homeData",{headers})
+    .pipe(map(
+      ( response )=> response
+   )).
+   subscribe((data) => this.setData(data));
+  }
+
+  setData(data) {
+     this.homeData = data["home"];
+  }
 }
--- src/app/mock-data.ts
+++ src/app/mock-data.ts
@@ -7,9 +7,13 @@ export interface HeaderMockData {
 }
 
 export interface HomeMockData {
-    title: string;
-    why: string;
-    our: string;
-    explore: string;
-    contact: string;
+    democratizing : string;
+    desc: string;
+    sub: string;
+    submit : string;
+    withOrsus : string;
+    creators : string;
+    artists : string;
+    fansfol : string;
+    fans: string;
 }
